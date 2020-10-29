4e3961936e2158a3e30f5d13ee9c10b66ded75ef
Rafael Santos <rafael.santos.bra@gmail.com>
2020-10-29 13:21:53 -0400

    Added files from Spring Initializr

---
+++ src/main/java/com/github/rafasantos/twiliosmssample/TwilioSmsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafasantos.twiliosmssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioSmsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioSmsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rafasantos/twiliosmssample/TwilioSmsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rafasantos.twiliosmssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioSmsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
