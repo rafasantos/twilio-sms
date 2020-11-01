e5662be9fcffa3e2070fce46bb16e3b74187319a
Rafael Santos <rafael.santos.bra@gmail.com>
2020-11-01 16:46:58 -0500

    Added send SMS to compose controller

--- src/main/java/com/github/rafasantos/twiliosmssample/ui/ComposeController.java
+++ src/main/java/com/github/rafasantos/twiliosmssample/ui/ComposeController.java
@@ -2,7 +2,9 @@ package com.github.rafasantos.twiliosmssample.ui;
 
 import com.github.rafasantos.twiliosmssample.account.AccountService;
 import com.github.rafasantos.twiliosmssample.sms.SmsService;
+import com.twilio.rest.api.v2010.account.Message;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 
 @Controller
 @RequestMapping("/compose")
 @AllArgsConstructor
+@Slf4j
 public class ComposeController {
     private final AccountService accountService;
     private final SmsService smsService;
@@ -25,20 +27,22 @@ public class ComposeController {
             @RequestParam("fromPhoneNumber") String fromPhoneNumber,
             @RequestParam("toPhoneNumber") String toPhoneNumber,
             @RequestParam("smsMessage") String smsMessage,
-            HttpServletRequest request,
             Model model) {
-
-//        smsService.sendSms()
-
-
-
+        try {
+            Message message = smsService.sendSms(toPhoneNumber, fromPhoneNumber, smsMessage);
+            log.info("SMS message sent; MessageSid: {}", message.getSid());
+            model.addAttribute("success", "Message sent. MessageSid: " + message.getSid());
+        } catch (Exception e) {
+            log.error("Error when sending message: {}", e.getMessage(), e);
+            model.addAttribute("error", "Error when sending message. " + e.getMessage());
+        }
         Set<String> phoneNumbers = accountService.findAllPhoneNumbers();
         model.addAttribute("phoneNumbers", phoneNumbers);
         return "compose-page";
     }
 
     @GetMapping
-    public String get(HttpServletRequest request, Model model) {
+    public String get(Model model) {
         Set<String> phoneNumbers = accountService.findAllPhoneNumbers();
         model.addAttribute("phoneNumbers", phoneNumbers);
         return "compose-page";
